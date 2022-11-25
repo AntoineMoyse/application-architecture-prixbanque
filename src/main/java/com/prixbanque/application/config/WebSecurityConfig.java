32089a7ad0766dd50da08543b1c43da4beecf248
AntoineMoyse <antoine.moyse@gmail.com>
2022-11-25 17:11:22 -0500

    Toujours rien qui marche, pas etonant security est une bouse phenomenale

--- src/main/java/com/prixbanque/application/config/WebSecurityConfig.java
+++ src/main/java/com/prixbanque/application/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,9 +18,8 @@ import com.prixbanque.application.service.LoginService;
  * @author antoine
  *
  */
-@SuppressWarnings("deprecation")
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	/**
