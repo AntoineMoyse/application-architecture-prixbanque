8dacc2c7674bf261621b748641916d1bfc89c467
AntoineMoyse <antoine.moyse@gmail.com>
2022-12-13 11:57:07 -0500

    jai essayé de faire des tests ça tourne mal explications

--- src/main/java/com/prixbanque/application/Application.java
+++ src/main/java/com/prixbanque/application/Application.java
@@ -1,8 +1,11 @@
 package com.prixbanque.application;
 
 import com.prixbanque.application.model.Solde;
+import com.prixbanque.application.model.Virement;
 import com.prixbanque.application.repository.ClientRepository.ClientRepository;
 import com.prixbanque.application.repository.SoldeRepository.SoldeRepository;
+import com.prixbanque.application.repository.VirementRepository.VirementRepository;
+import com.prixbanque.application.service.VirementService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +14,12 @@ import org.springframework.context.annotation.Bean;
 import com.prixbanque.application.model.Role;
 import com.prixbanque.application.repository.ClientRepository.RoleRepository;
 import com.prixbanque.application.model.Client;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -32,8 +39,7 @@ public class Application{
 	}
 
 	@Bean
-	static
-	CommandLineRunner init(RoleRepository roleRepository, ClientRepository clientrepo, SoldeRepository solderepo) {
+	CommandLineRunner init(RoleRepository roleRepository, ClientRepository clientrepo, SoldeRepository solderepo, VirementRepository virementrepo, VirementService virementservice) {
 
 	    return args -> {
 
@@ -51,21 +57,77 @@ public class Application{
 	            roleRepository.save(newUserRole);
 	        }
 
-			Client client = clientrepo.findClientBymailadress("user.test@dum.com");
-			if(client == null){
+			Client client1 = clientrepo.findClientBymailadress("user.test@dum.com");
+			Client client2 = clientrepo.findClientBymailadress("test.user@dum.com");
+			if(client1 == null){
 				BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-				Client newclient = new Client();
-				newclient.setPwd(bCryptPasswordEncoder.encode("password"));
-				newclient.setRole(new HashSet<>(Arrays.asList(userRole)));
-				newclient.setFirstname("User");
-				newclient.setLastname("Test");
-				newclient.setMailadress("user.test@dum.com");
-				clientrepo.save(newclient);
+				Client newclient1 = new Client();
+				newclient1.setPwd(bCryptPasswordEncoder.encode("password"));
+				newclient1.setRole(new HashSet<>(Arrays.asList(userRole)));
+				newclient1.setFirstname("User");
+				newclient1.setLastname("Test");
+				newclient1.setMailadress("user.test@dum.com");
+				clientrepo.save(newclient1);
 
-				Solde newsolde = new Solde(50000.00f, newclient);
-				solderepo.save(newsolde);
+				Solde newsolde1 = new Solde(50000.00f, newclient1);
+				solderepo.save(newsolde1);
 			}
-	    };
+			if(client2 == null){
+				BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+				Client newclient2 = new Client();
+				newclient2.setPwd(bCryptPasswordEncoder.encode("password"));
+				newclient2.setRole(new HashSet<>(Arrays.asList(userRole)));
+				newclient2.setFirstname("Test");
+				newclient2.setLastname("User");
+				newclient2.setMailadress("test.user@dum.com");
+				clientrepo.save(newclient2);
 
+				Solde newsolde2 = new Solde(50000.00f, newclient2);
+				solderepo.save(newsolde2);
+			}
+
+			maketest(client1, client2, virementrepo, virementservice);
+	    };
+	}
+
+	public void maketest(Client client1, Client client2, VirementRepository virementrepo, VirementService virementservice){
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		ArrayList<Virement> listevirement = new ArrayList<Virement>();
+
+		listevirement = makelist(listevirement, client1, client2);
+
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("début test" + dtf.format(now));
+
+		//makeaddingtest(listevirement, virementrepo);
+		makeaddingtestservice(listevirement, virementservice);
+
+		now = LocalDateTime.now();
+		System.out.println("fin test" + dtf.format(now));
+	}
+
+	@Async
+	public ArrayList<Virement> makelist(ArrayList<Virement> listevirement, Client client1, Client client2){
+		for(int i = 0; i<2000; i++){
+			Virement virement1 = new Virement(1000, client1, client2);
+			Virement virement2 = new Virement(999, client2, client1);
+			listevirement.add(virement1);
+			listevirement.add(virement2);
+		}
+		return listevirement;
+	}
+
+	@Async
+	public void makeaddingtest(ArrayList<Virement> listevirement, VirementRepository virementrepo){
+		for(Virement virements : listevirement){
+			virementrepo.save(virements);
+		}
+	}
+
+	@Async
+	public void makeaddingtestservice(ArrayList<Virement> listevirement, VirementService virementservice){
+		for(Virement virements : listevirement){
+			virementservice.newVirement(virements);
+		}
 	}
 }
